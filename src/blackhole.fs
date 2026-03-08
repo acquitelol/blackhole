ef752d4107e389cf20468707bcb0556661559f5a
Rosie <acquitefx@gmail.com>
2026-03-08 22:59:19 +0000

    feat: gravitational lensing

--- src/blackhole.fs
+++ src/blackhole.fs
@@ -1,9 +1,26 @@
 #version 330
 
 in vec2 fragTexCoord;
-out vec4 finalColor;
+out vec4 fragColor;
+
 uniform sampler2D texture0;
+uniform vec2 blackhole;
+uniform vec2 resolution;
+
+const float STRENGTH = 3000000.0;
 
 void main() {
-    finalColor = texture(texture0, fragTexCoord);
+    vec2 uv = fragTexCoord;
+    vec2 pos = uv * resolution;
+
+    vec2 d = pos - blackhole;
+    float dist = length(d) + 0.001;
+
+    float offset = STRENGTH / (dist * dist + 1.0);
+    vec2 lensed_pos = pos - (d / dist) * offset;
+    vec2 lensed_uv = lensed_pos / resolution;
+    lensed_uv = clamp(lensed_uv, 0.0, 1.0);
+
+    lensed_uv.y = 1.0 - lensed_uv.y;
+    fragColor = texture(texture0, lensed_uv);
 }
